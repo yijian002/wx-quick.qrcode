27c7cbce341c3a4c0706120c144f8a6dc8b47dfe
yijian002 <yijian002@gmail.com>
2017-08-31 21:24:49 +0800

    update page

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,8 +1,30 @@
-//index.js
+
 //获取应用实例
 const app = getApp()
 
-let CACHE_QRCODE_LIST = 'qkqrcode_user_list';
+var util = require('../../utils/util.js')
+var config = require('../../utils/config.js')
+
+const setList = list => {
+    let nlist = {};
+    for(let i = 0, len = list.length; i < len; i++) {
+        let k = '' + list[i].year + list[i].month + list[i].day
+        if (!nlist[k]) {
+            nlist[k] = [];
+        }
+        nlist[k].push(list[i]);
+    }
+
+    let arr = [];
+    for (let k in nlist) {
+        arr.push(nlist[k]);
+    }
+    console.log(arr)
+    return arr;
+    
+}
+
+var qrcode_list = [];
 
 Page({
     data: {
@@ -14,13 +36,20 @@ Page({
         wx.scanCode({
             onlyFromCamera: true,
             success: (res) => {
-                // console.log(res.result);
-                this.data.qrcodeList = [{ content: res.result }].concat(this.data.qrcodeList);
-                this.setData({ qrcodeList: this.data.qrcodeList });
-                wx.setStorage({
-                    key: CACHE_QRCODE_LIST,
-                    data: this.data.qrcodeList,
-                })
+              let times = new Date();
+              qrcode_list = [{ 
+                content: res.result,
+                year: times.getFullYear(),
+                month: times.getMonth() + 1,
+                day: util.formatNumber(times.getDate()),
+                time: util.formatNumber(times.getHours()) + ':' + util.formatNumber(times.getMinutes())
+              }].concat(qrcode_list);
+
+              this.setData({ qrcodeList: setList(qrcode_list) });
+              wx.setStorage({
+                key: config.CACHE.list_qrcodes,
+                data: qrcode_list
+              })
             }
         });
     },
@@ -40,12 +69,11 @@ Page({
         }
 
         wx.getStorage({
-            key: CACHE_QRCODE_LIST,
-            success: function(res) {
-                this.setData({
-                    qrcodeList: res || []
-                });
-            }
+          key: config.CACHE.list_qrcodes,
+          success: function(res) {
+              qrcode_list = res.data || []
+              this.setData({ qrcodeList: setList(qrcode_list) });
+          }
         })
 
     }
---
+++ utils/config.js
@@ -0,0 +1,9 @@
+const KEY = 'quick_qrcode_'
+
+const CACHE = {
+  list_qrcodes: KEY + 'list_qrcodes'
+}
+
+module.exports = {
+  CACHE: CACHE
+}
