f5c1e4780d7ba4dfec5e04d66dee0841dc0f229c
yijian002 <yijian002@gmail.com>
2017-08-31 14:37:22 +0800

    Init page

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,53 +2,51 @@
 //获取应用实例
 const app = getApp()
 
+let CACHE_QRCODE_LIST = 'qkqrcode_user_list';
+
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
+    data: {
+        hasUserInfo: false,
+        userInfo: {},
+        qrcodeList: []
+    },
+    cameraQrcode: function() {
+        wx.scanCode({
+            onlyFromCamera: true,
+            success: (res) => {
+                // console.log(res.result);
+                this.data.qrcodeList = [{ content: res.result }].concat(this.data.qrcodeList);
+                this.setData({ qrcodeList: this.data.qrcodeList });
+                wx.setStorage({
+                    key: CACHE_QRCODE_LIST,
+                    data: this.data.qrcodeList,
+                })
+            }
+        });
+    },
+    onLoad: function() {
+        if (app.globalData.userInfo) {
+            this.setData({
+                userInfo: app.globalData.userInfo,
+                hasUserInfo: true
+            })
+        } else {
+            app.userInfoReadyCallback = res => {
+                this.setData({
+                    userInfo: res.userInfo,
+                    hasUserInfo: true
+                })
+            }
         }
-      })
+
+        wx.getStorage({
+            key: CACHE_QRCODE_LIST,
+            success: function(res) {
+                this.setData({
+                    qrcodeList: res || []
+                });
+            }
+        })
+
     }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
 })
