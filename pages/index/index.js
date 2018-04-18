a7bf06dfd80565ebf3e2bf6782ca4d40effe0120
yijian002 <yijian002@gmail.com>
2018-04-18 10:20:20 +0800

    Day One

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,77 +5,39 @@ const app = getApp()
 var util = require('../../utils/util.js')
 var config = require('../../utils/config.js')
 
-const setList = list => {
-    let nlist = {};
-    for(let i = 0, len = list.length; i < len; i++) {
-        let k = '' + list[i].year + list[i].month + list[i].day
-        if (!nlist[k]) {
-            nlist[k] = [];
-        }
-        nlist[k].push(list[i]);
-    }
-
-    let arr = [];
-    for (let k in nlist) {
-        arr.push(nlist[k]);
-    }
-    // console.log(arr)
-    return arr;
-    
-}
-
-var qrcode_list = [];
+var list = [];
 
 Page({
     data: {
         hasUserInfo: false,
         userInfo: {},
-        qrcodeList: []
+        list: list
     },
-    cameraQrcode: () => {
-        wx.scanCode({
-            onlyFromCamera: true,
+    tapCamera: function() {
+        var _this = this
+        wx.chooseImage({
+            count: 1,
+            sizeType: ['original'],
+            sourceType: ['camera'],
             success: (res) => {
-              qrcode_list = [{ 
-                content: res.result,
-                year: times.getFullYear(),
-                month: times.getMonth() + 1,
-                day: util.formatNumber(times.getDate()),
-                time: util.formatNumber(times.getHours()) + ':' + util.formatNumber(times.getMinutes())
-              }].concat(qrcode_list);
-
-              this.setData({ qrcodeList: setList(qrcode_list) });
-              wx.setStorage({
-                key: config.CACHE.list_qrcodes,
-                data: qrcode_list
-              })
-            }
-        });
-    },
-    onLoad: () => {
-        if (app.globalData.userInfo) {
-            this.setData({
-                userInfo: app.globalData.userInfo,
-                hasUserInfo: true
-            })
-        } else {
-            app.userInfoReadyCallback = res => {
-                this.setData({
-                    userInfo: res.userInfo,
-                    hasUserInfo: true
+                console.log(res.tempFilePaths[0])
+                _this.setData({
+                    list: list.push({ pic: res.tempFilePaths[0] })
                 })
             }
-        }
-
+        })
+    },
+    onLoad: () => {
+        // wx.getStorage({
+        //     key: config.CACHE.list_qrcodes,
+        //     success: (res) => {
+        //         console.log(res)
+        //         qrcode_list = res.data || []
+        //         // console.log(qrcode_list)
+        //         this.setData({ qrcodeList: setList(qrcode_list) });
+        //     }
+        // })
     },
     onReady: () => {
-        wx.getStorage({
-            key: config.CACHE.list_qrcodes,
-            success: function (res) {
-                qrcode_list = res.data || []
-                console.log(qrcode_list)
-                this.setData({ qrcodeList: setList(qrcode_list) });
-            }
-        })
     }
 })
